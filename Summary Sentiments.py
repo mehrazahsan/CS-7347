4c0ada0a5501937a0e59dc101f4b9904cd0902e3
Mohammed Ahsan <mehrazahsan@gmail.com>
2024-11-16 18:26:55 -0500

    Working summarization using the BERT extractive summarization model from Derek Millers paper on summarizing lecture infomration

--- Summary Sentiments.py
+++ Summary Sentiments.py
@@ -3,4 +3,28 @@
 
 # Code for An Overview of Sentiments Expressed Across Abstractive and Extractive Summaries
 
+import random,json,torch
+from datasets import load_dataset
+from summarizer import Summarizer
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# random.seed(42) ensures we get the same random documents. 
+dataset = load_dataset("cnn_dailymail", "3.0.0")
+total_articles=dataset['train']
+articles=random.sample(list(total_articles), 100)
+
+
+model=Summarizer()
+summaries = []
+
+for article in articles:
+    text = article['article']  # Extract the article text
+    summary = model(text, min_length=60, max_length=300)
+    summaries.append({'article': text, 'summary': summary})
+    
+with open("summaries.json", "w") as file:
+    json.dump(summaries, file, indent=4)
+
+print('Summaries are pushed to json')
 
